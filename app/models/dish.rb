6aa0be6e0204b05e6f5eafab13018ad07fe4fb7e
carolineglass <carolineglass89@gmail.com>
2020-10-18 20:34:18 -0400

    Done.

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -2,4 +2,8 @@ class Dish < ActiveRecord::Base
     belongs_to :restaurant
     has_many :dish_tags
     has_many :tags, through: :dish_tags
+
+    validates :name, presence: true
+    validates :restaurant, presence: true
+    
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,6 @@
 class Tag < ActiveRecord::Base
     has_many :dish_tags
-    has_many :tags, through: :dish_tags
+    has_many :dishes, through: :dish_tags
+
+    validates :name, length: { minimum: 3 }
 end
